73509a76556795f8415f24acb841f2d06098853d
franciscotov <frank.ronaldo_17@hotmail.com>
2021-06-10 21:54:03 -0300

    resuelvo conflicto d overflow

--- portfolio/src/components/Iconos.jsx
+++ portfolio/src/components/Iconos.jsx
@@ -33,62 +33,7 @@ import React, { useRef } from 'react';
 function Iconos () {
     return(
         <div>Iconos</div>
-    ); 
-    // let mount = useRef(null);
-    // const color3 = new THREE.Color("rgb(255, 255, 0)");
-    // // ************************
-    // const scene = new THREE.Scene();
-    // scene.background = {color: color3}
-    // // const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    // const camera = new THREE.PerspectiveCamera( 20, 1, 0.1, 1000 );
-    // const renderer = new THREE.WebGLRenderer( {alpha : 0.2});
-    // renderer.clear(true, false, false);
-    // renderer.setClearAlpha ( 0.0 );
-    // renderer.setClearColor ( {color : 0x00ff00 })
-    // renderer.setSize( 200, 200 );
-    // console.log(window.innerWidth, 'tttt')
-    // const geometry = new THREE.BoxGeometry();
-    // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, transparent: true } );
-    // const cube = new THREE.Mesh( geometry, material );
-    // scene.add( cube );
-    // camera.position.z = 5;
-    // function animate() {
-    //     requestAnimationFrame( animate );
-    //     // renderer.render( scene, camera );
-    //     cube.rotation.x += 0.01;
-    //     cube.rotation.y += 0.01;
-    //     renderer.render( scene, camera );
-    // }
-    // animate();
-    // React.useEffect(() => {
-    //     // const myDiv = document.createElement("div");
-    //     // var newContent = document.createTextNode("Hola!¿Qué tal?");
-    //     renderer.autoClear = true;
-    //     renderer.info.autoReset = false;
-    //     renderer.info.reset();
-    //     try{
-    //         mount.removeChild(renderer.domElement)
-    //     }catch(e){
-    //         console.log('no habia hijo')
-    //     }
-    //     mount.appendChild(renderer.domElement);
-    //     // const elem = useRef(myDiv);
-    //     // console.log(elem.current)
-    //     // myDiv.appendChild(renderer.domElement);
-    //     // console.log(myDiv,'yyyyyyy')
-    //     // const elem = renderer.domElement;
-    //     // console.log(iconosContainer, 'asttatstats', typeof renderer.domElement)
-    //     // Component.mount.appendChild( renderer.domElement );
-    //     // iconosContainer.append(renderer.domElement)
-        
-    // }, [])
-    
-    // return (
-    //     // <Elem />
-    //     <div ref={ref => (mount = ref)} />
-    //     // <div ref ={elem} />
-            
-    // );
+    );
 };
 
 export default Iconos;
