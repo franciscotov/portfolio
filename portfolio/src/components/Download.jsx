eed915f20f632e1082267cc73c62d210e95c8a33
franciscotov <frank.ronaldo_17@hotmail.com>
2023-11-06 20:46:08 -0300

    ajuste de archivos en general

--- portfolio/src/components/Download.jsx
+++ portfolio/src/components/Download.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Download.module.scss";
+import spanishFile from "./CV_Francisco_Tovar.pdf"
+import englishFile from "./CVI_Francisco_Tovar.pdf"
 
 const Download = ({ state }) => {
   return (
     <div className={styles.containerDownload}>
       <a
-        href={state ? "CV_Francisco_Tovar.pdf" : "CV_Francisco_Tovar.pdf"}
+        href={state ? englishFile : spanishFile}
         download
       >
         {state ? "Download CV" : "Descargar CV"}
--- portfolio/src/components/Images.jsx
+++ portfolio/src/components/Images.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import styles from "./Images.module.scss";
+import web_development from "../assets/images/web_development.png";
+import sports from "../assets/images/sports.png";
+import responsive from "../assets/images/responsive.png";
+import atom from "../assets/images/atom.png";
 
 const Images = ({ state }) => {
   let listOfImages = [
     {
       description: state ? "development" : "desarrollo",
-      img: "web_development.png",
+      img: web_development,
     },
-    { description: state ? "sports" : "deportes", img: "sports.png" },
-    { description: "responsive", img: "responsive.png" },
-    { description: state ? "science" : "ciencias", img: "atom.png" },
+    { description: state ? "sports" : "deportes", img: sports },
+    { description: "responsive", img: responsive },
+    { description: state ? "science" : "ciencias", img: atom },
   ];
 
   return (
     <div className={styles.container}>
       {listOfImages.map((ele, index) => (
         <div className={styles.containerImg} key={`img-${index}`}>
-          <img
-            src={require(`../assets/images/${ele.img}`).default}
-            alt=""
-            className={styles.images}
-          />
+          <img src={ele.img} alt="" className={styles.images} />
           <span className={styles.description}>
             {ele.description.toUpperCase()}
           </span>
