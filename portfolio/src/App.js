c708b9a58beec2e9b3ae8da9121eba8f0785bbc4
franciscotov <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2025-05-03 00:31:10 -0300

    adding loader

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,14 +7,25 @@ import Portfolio from "@/components/Portfolio";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Navbar from "@/components/Navbar";
-import styles from "@/App.module.scss";
+import styles from "@/styles.module.scss";
 import Footer from "@/components/Footer";
 import "@/config/i18n";
+import { InfinitySpin } from "react-loader-spinner";
 
 function App() {
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className={styles.containerLoading}>
+          <InfinitySpin
+            visible={true}
+            width="200"
+            color="#00486B"
+            ariaLabel="infinity-spin-loading"
+          />
+        </div>
+      }
+    >
       <div className={styles.App}>
         <Navbar />
         <Home />
