b8ea47ebba2c25aa13f1f1095a9123f2b801aa8a
franciscotov <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2025-07-15 14:47:48 -0300

    adding craco configs for styles

--- portfolio/craco.config.js
+++ portfolio/craco.config.js
@@ -5,4 +5,38 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  style: {
+    modules: {
+      localIdentName: '',
+    },
+    css: {
+      // loaderOptions: { /* ... */ },
+      loaderOptions: (cssLoaderOptions, { env, paths }) => {
+        /* ... */
+        return cssLoaderOptions;
+      },
+    },
+    sass: {
+      // loaderOptions: { /* ... */ },
+      loaderOptions: (sassLoaderOptions, { env, paths }) => {
+        /* ... */
+        return sassLoaderOptions;
+      },
+    },
+    postcss: {
+      mode: 'extends' /* (default value) */ || 'file',
+      // plugins: [require('plugin-to-append')],
+      // plugins: (plugins) => [require('plugin-to-prepend')].concat(plugins),
+      // env: {
+      //   autoprefixer: { /* ... */ },
+      //   stage: 3,
+      //   features: { /* ... */ },
+      // },
+      // loaderOptions: { /* ... */ },
+      // loaderOptions: (postcssLoaderOptions, { env, paths }) => {
+      //   /* ... */
+      //   return postcssLoaderOptions;
+      // },
+    },
+  },
 };
